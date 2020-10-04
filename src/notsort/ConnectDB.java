776c9ff4ceeaf9e7db7ed110a6e7a8f3ef9d1d8d
TH0RMAS <puppyekal@naver.com>
2020-10-05 01:09:50 +0900

    DB 테이블 생성 추가

--- src/notsort/ConnectDB.java
+++ src/notsort/ConnectDB.java
@@ -3,13 +3,11 @@ package notsort;
 import java.sql.*;
 
 public class ConnectDB {
-    public Statement stmt;
-    public PreparedStatement pstmt = null;
-
-    public ResultSet rs;
-
     public static Connection GetDB() {
         Connection con = null;
+        PreparedStatement pstmt = null;
+        Statement stmt = null;
+        ResultSet rs;
         String url = "jdbc:mysql://127.0.0.1:3306/charts_db?&serverTimezone=Asia/Seoul&useSSL=false";
         String userid = "root";
         String pwd = "root";
@@ -29,6 +27,31 @@ public class ConnectDB {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
+        try{
+            stmt = con.createStatement();
+            String sql = "SELECT table_name FROM information_schema.tables where table_schema = 'charts_db' and table_name = 'songinfo'";
+            rs = stmt.executeQuery(sql);
+
+            if(!rs.next()){
+                System.out.println("데이터베이스 테이블 생성");
+                //테이블이 없다면 생성
+
+                sql = "create table songinfo ("
+                        + "`title` varchar(100) COLLATE utf8_bin NOT NULL,"
+                        + "`artist` varchar(100) COLLATE utf8_bin NOT NULL,"
+                        + "`albumname` varchar(100) COLLATE utf8_bin NOT NULL,"
+                        + "`siteid` int NOT NULL,"
+                        + "`comment` varchar(100) COLLATE utf8_bin NOT NULL,"
+                        + "`pwd` varchar(8) COLLATE utf8_bin NOT NULL"
+                        + ")";
+                boolean rs2 = stmt.execute(sql);
+                stmt.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
         return con;
     }
 }
