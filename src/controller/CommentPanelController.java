5d5275f0429286dcda69940f98f2ded9cc58abc7
LeftMiddleRight <dlsrks0112@naver.com>
2020-11-06 15:22:07 +0900

    [MVC 분리] 멤버변수 _추가 및 형식 통일
    멤버변수 _추가 및 형식 통일

--- src/controller/CommentPanelController.java
+++ src/controller/CommentPanelController.java
@@ -16,46 +16,46 @@ public class CommentPanelController {
     ResultSet rs = null;
 
 
-    private CommentPanel theCommentPanel;
+    private CommentPanel the_Comment_Panel;
 
     public CommentPanelController(CommentPanel theCommentPanel) {
-        this.theCommentPanel = theCommentPanel;
-        this.theCommentPanel.addBtnRegisterListener(new ButtonRegisterListener());
-        this.theCommentPanel.addBtnDeleteListener(new ButtonDeleteListener());
-        this.theCommentPanel.addBtnBackListener(new ButtonBackListener());
+        this.the_Comment_Panel = theCommentPanel;
+        this.the_Comment_Panel.addBtnRegisterListener(new ButtonRegisterListener());
+        this.the_Comment_Panel.addBtnDeleteListener(new ButtonDeleteListener());
+        this.the_Comment_Panel.addBtnBackListener(new ButtonBackListener());
     }
 
 
     private class ButtonRegisterListener implements ActionListener{
-        private Component _viewLoading;
+        private Component view_Loading;
         public ButtonRegisterListener() { }
         public ButtonRegisterListener(Component parentComponent){
-            _viewLoading = parentComponent;
+            view_Loading = parentComponent;
         }
 
         @Override
         public void actionPerformed(ActionEvent e) {
-            if (!theCommentPanel.txtComment.getText().equals("")) {
+            if (!the_Comment_Panel.txtComment.getText().equals("")) {
                 con = ConnectDB.GetDB();
                 try {
                     String sql = "INSERT INTO songinfo VALUES (?, ?, ?, ?, ?, ?)";
                     pstmt = con.prepareStatement(sql);
-                    pstmt.setString(1, theCommentPanel.sqltitle);
-                    pstmt.setString(2, theCommentPanel.strArtist);
-                    pstmt.setString(3, AppManager.getS_instance().getParser().getAlbumName(theCommentPanel.strTitle));
+                    pstmt.setString(1, the_Comment_Panel.sqltitle);
+                    pstmt.setString(2, the_Comment_Panel.strArtist);
+                    pstmt.setString(3, AppManager.getS_instance().getParser().getAlbumName(the_Comment_Panel.strTitle));
                     pstmt.setInt(4, AppManager.getS_instance().getSite_M_B_G());
-                    pstmt.setString(5, theCommentPanel.txtComment.getText());
-                    pstmt.setString(6, theCommentPanel.txtPassword.getText());
+                    pstmt.setString(5, the_Comment_Panel.txtComment.getText());
+                    pstmt.setString(6, the_Comment_Panel.txtPassword.getText());
                     pstmt.executeUpdate();
-                    theCommentPanel.modelList.addElement(theCommentPanel.txtComment.getText());
+                    the_Comment_Panel.modelList.addElement(the_Comment_Panel.txtComment.getText());
 
-                    theCommentPanel.arrComment.add(theCommentPanel.txtComment.getText());
-                    if (theCommentPanel.txtPassword.getText().equals(""))
-                        theCommentPanel.arrPassword.add("0000");
+                    the_Comment_Panel.arrComment.add(the_Comment_Panel.txtComment.getText());
+                    if (the_Comment_Panel.txtPassword.getText().equals(""))
+                        the_Comment_Panel.arrPassword.add("0000");
                     else
-                        theCommentPanel.arrPassword.add(theCommentPanel.txtPassword.getText());
-                    theCommentPanel.txtComment.setText("");
-                    theCommentPanel.txtPassword.setText("");
+                        the_Comment_Panel.arrPassword.add(the_Comment_Panel.txtPassword.getText());
+                    the_Comment_Panel.txtComment.setText("");
+                    the_Comment_Panel.txtPassword.setText("");
                 } catch (SQLException e1) {
                     e1.printStackTrace();
                 }
@@ -64,45 +64,45 @@ public class CommentPanelController {
     }//ButtonRegisterListener
 
     private class ButtonDeleteListener implements ActionListener{
-        private Component _viewLoading;
+        private Component view_Loading;
         public ButtonDeleteListener() { }
         public ButtonDeleteListener(Component parentComponent){
-            _viewLoading = parentComponent;
+            view_Loading = parentComponent;
         }
 
         @Override
         public void actionPerformed(ActionEvent e) {
-            if (Integer.parseInt(theCommentPanel.txtPassword.getText()) == Integer.parseInt(theCommentPanel.arrPassword.get(theCommentPanel.listComment.getSelectedIndex()))) {
-                System.out.println("Same Password! At : " + String.valueOf(theCommentPanel.listComment.getSelectedIndex()));
-                theCommentPanel.con = ConnectDB.GetDB();
+            if (Integer.parseInt(the_Comment_Panel.txtPassword.getText()) == Integer.parseInt(the_Comment_Panel.arrPassword.get(the_Comment_Panel.listComment.getSelectedIndex()))) {
+                System.out.println("Same Password! At : " + String.valueOf(the_Comment_Panel.listComment.getSelectedIndex()));
+                the_Comment_Panel.con = ConnectDB.GetDB();
                 try {
-                    theCommentPanel.arrPassword.remove(theCommentPanel.listComment.getSelectedIndex());
-                    theCommentPanel.arrComment.remove(theCommentPanel.listComment.getSelectedIndex());
-                    theCommentPanel.modelList.removeElementAt(theCommentPanel.listComment.getSelectedIndex());
+                    the_Comment_Panel.arrPassword.remove(the_Comment_Panel.listComment.getSelectedIndex());
+                    the_Comment_Panel.arrComment.remove(the_Comment_Panel.listComment.getSelectedIndex());
+                    the_Comment_Panel.modelList.removeElementAt(the_Comment_Panel.listComment.getSelectedIndex());
                     String sql = "DELETE FROM songinfo WHERE title = ? AND pwd = ?";
                     pstmt = con.prepareStatement(sql);
-                    pstmt.setString(1, theCommentPanel.sqltitle);
-                    pstmt.setString(2, theCommentPanel.txtPassword.getText());
+                    pstmt.setString(1, the_Comment_Panel.sqltitle);
+                    pstmt.setString(2, the_Comment_Panel.txtPassword.getText());
                     int temp = pstmt.executeUpdate();
                 } catch (SQLException e1) {
                     e1.printStackTrace();
                 }
             }
 
-            theCommentPanel.txtPassword.setText("");
+            the_Comment_Panel.txtPassword.setText("");
         }//actionPerfomed
     }//ButtonDeleteListener
 
     private class ButtonBackListener implements ActionListener{
-        private Component _viewLoading;
+        private Component view_Loading;
         public ButtonBackListener() { }
         public ButtonBackListener(Component parentComponent){
-            _viewLoading = parentComponent;
+            view_Loading = parentComponent;
         }
 
         @Override
         public void actionPerformed(ActionEvent e) {
-            theCommentPanel.clearMusicData();
+            the_Comment_Panel.clearMusicData();
             AppManager.getS_instance().BackToChartPrimaryPanel();
             System.out.println("Back To ChartPrimary");
         }//actionPerfomed
