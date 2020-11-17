9a74f4727f652b32946820f830bf0f7156bb5113
Inho Choi <eknow25@gmail.com>
2020-11-17 21:38:38 +0900

    Remove KeyactionListener

--- src/view/SiteChartsPanel.java
+++ src/view/SiteChartsPanel.java
@@ -104,7 +104,6 @@ public class SiteChartsPanel extends JPanel{
         txtSearch = new JTextField();
         txtSearch.setBounds(250,30,840,40);
         txtSearch.setFont(new Font("SansSerif", Font.PLAIN, 25));
-        txtSearch.addKeyListener(new KeyActionListener());
         this.add(txtSearch);
     }
 
@@ -169,98 +168,18 @@ public class SiteChartsPanel extends JPanel{
         public void actionPerformed(ActionEvent e) {
             Object obj = e.getSource();
             if (obj == btnRefresh) {
-
-                /*
-                current = LocalDateTime.now();
-                formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
-                switch (AppManager.getS_instance().getSite_M_B_G()){
-                    case 1:
-                        formatted_Melon = current.format(formatter);
-                        lblTime.setText("Renewal time : " + formatted_Melon);
-                        AppManager.getS_instance().setSite_M_B_G(1);
-                        AppManager.getS_instance().DataPassing(_viewLoading);
-                        System.out.println("why?");
-                        break;
-                    case 2:
-                        formatted_Bugs = current.format(formatter);
-                        lblTime.setText("Renewal time : " + formatted_Bugs);
-                        AppManager.getS_instance().setSite_M_B_G(2);
-                        AppManager.getS_instance().DataPassing(_viewLoading);
-                        break;
-                    case 3:
-                        formatted_Genie = current.format(formatter);
-                        lblTime.setText("Renewal time : " + formatted_Genie);
-                        AppManager.getS_instance().setSite_M_B_G(3);
-                        AppManager.getS_instance().DataPassing(_viewLoading);
-                        break;
-                }
-                */
-
             }//refresh 새로 파싱해옴//파싱시간도 갱신
             if (obj == btnSite_Melon) {
-                /*
-                if(AppManager.getS_instance().getSite_M_B_G() == 1) return;
-                AppManager.getS_instance().setSite_M_B_G(1);
-                if(!AppManager.getS_instance().getParser().isParsed()) AppManager.getS_instance().DataPassing(_viewLoading);
-                System.out.println("Melon");
-                pnlSitePanel.changeData();
-                lblTime.setText("Renewal time : " + formatted_Melon);
-                txtSearch.setText("");
-                pnlSitePanel.filter(null,2);
-                */
+
             }
             if (obj == btnSite_Bugs) {
-                /*
-                if(AppManager.getS_instance().getSite_M_B_G() == 2) return;
-                AppManager.getS_instance().setSite_M_B_G(2);
-                if(!AppManager.getS_instance().getParser().isParsed()) AppManager.getS_instance().DataPassing(_viewLoading);
-                System.out.println("Bugs");
-                pnlSitePanel.changeData();
-                lblTime.setText("Renewal time : " + formatted_Bugs);
-                txtSearch.setText("");
-                pnlSitePanel.filter(null,2);
-                */
+
             }
             if (obj == btnSite_Genie) {
-                /*
-                if(AppManager.getS_instance().getSite_M_B_G() == 3) return;
-                AppManager.getS_instance().setSite_M_B_G(3);
-                if(!AppManager.getS_instance().getParser().isParsed()) AppManager.getS_instance().DataPassing(_viewLoading);
-                System.out.println("Genie");
-                pnlSitePanel.changeData();
-                lblTime.setText("Renewal time : " + formatted_Genie);
-                txtSearch.setText("");
-                pnlSitePanel.filter(null,2);
-                */
+
             }
-            //멜론, 벅스, 지니 버튼에 따라 저장해 놓은 데이터를 새로 갖고옴
-            //저장되있던 불러온 시간도 같이 갖고옴
         }
     }//ButtonListener
 
-    private class KeyActionListener implements KeyListener{
 
-        @Override
-        public void keyTyped(KeyEvent e) { }
-
-        @Override
-        public void keyPressed(KeyEvent e) { }
-
-        @Override
-        public void keyReleased(KeyEvent e) {
-            Object obj = e.getSource();
-
-            if(obj == txtSearch){
-                /*
-                //strSearchCategory = {"Name", "Artist"};
-                if(0 == strCombo.getSelectedIndex())//Name
-                    pnlSitePanel.filter(txtSearch.getText(),2);
-                if(1 == strCombo.getSelectedIndex())//Artist
-                    pnlSitePanel.filter(txtSearch.getText(),3);
-                */
-            }//comboBox 0, 1일때 sitepanel의 filter에서 검색
-
-        }//KeyReleased
-
-    }//KeyListener
 }
