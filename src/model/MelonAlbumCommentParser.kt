f16003d25d9d63cb9848b6b4a635f7189d05d7a0
Inho Choi <eknow25@gmail.com>
2020-11-25 18:39:47 +0900

    Finish MelonAlbumCommentParser.kt Alpha Version

--- src/model/MelonAlbumCommentParser.kt
+++ src/model/MelonAlbumCommentParser.kt
@@ -1,57 +1,74 @@
 package model
 
+import main.AppManager
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.chrome.ChromeDriver
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
 
-class MelonAlbumCommentParser : CommentParser{
+class MelonAlbumCommentParser(var driver: WebDriver) {
 	//Properties
-	private var driver: WebDriver? = null
 	private val WEB_DRIVER_ID = "webdriver.chrome.driver"
 	private val WEB_DRIVER_PATH = "src/driver/chromedriver.exe"
-	private var base_url: String? = null
+	private var base_url: String? = "https://www.melon.com/album/detail.htm?albumId="
 
 	init {
 		System.setProperty(WEB_DRIVER_ID, WEB_DRIVER_PATH)
-		driver = ChromeDriver()
 	}
 
-	override fun crawl() {
+	fun crawl() : MutableMap<String,List<String>>{
+		val result = mutableMapOf<String, List<String>>()
+		val _setAlbumID = mutableSetOf<String>()
+
 		if (base_url == null) {
 			println("Url is null")
-			return
+			return result
 		}
+
+		for (i in 1..100) {
+			if(!ChartData.getS_instance().melonChartParser.isParsed)
+				ChartData.getS_instance().melonChartParser.chartDataParsing(null)
+			_setAlbumID.add(ChartData.getS_instance().melonChartParser.getAlbumID(i).filter { it in '0'..'9' })
+		}
+
 		try {
-			//get page (= 브라우저에서 url을 주소창에 넣은 후 request 한 것과 같다)
-			driver!!.get(base_url)
-			val html = driver!!.pageSource
-			val doc: Document = Jsoup.parseBodyFragment(html)
-			//print(doc.body().select("div.cmt_cont"))
-			val arr = doc.body().select("div.cntt").text().split("내용").toMutableList()
-			arr.forEach {
-				if (it.contains("재생 다운로드 곡명")) {
-					val index = it.indexOf("재생 다운로드 곡명")
-					print("++++")
-					print(it.substring(0,index))
-					println("++++")
-				}
+			var doc: Document
+			var arr: MutableList<String>
+			var html: String
+
+			for (id in _setAlbumID) {
+				driver.get(base_url + id)
+				html = driver.pageSource
+				doc = Jsoup.parseBodyFragment(html)
+				arr = doc.body().select("div.cntt").text().split("내용").toMutableList()
+				println(id)
+				result[id] = arr.refine()
 			}
-			for (ele in arr.subList(3, arr.lastIndex - 1)) {
-				println(ele)
+
+			for (ele in result) {
+				println("KEY : ${ele.key}")
+				for (str in ele.value)
+					println(str)
 			}
-			//System.out.println(driver.getPageSource());
+
 		} catch (e: Exception) {
 			e.printStackTrace()
-		} finally {
-			driver!!.close()
 		}
+
+		return result
 	}
 
-	fun setAlbumUrl(str: String): Unit {
-		base_url = "https://www.melon.com/album/detail.htm?albumId=$str";
+
+	fun MutableList<String>.refine(): MutableList<String> {
+		this.replaceAll {
+			if (it.contains("재생 다운로드 곡명"))
+				it.substring(5, it.indexOf("재생 다운로드 곡명"))
+			else if (it.contains("NEW"))
+				it.substring(5)
+			else
+				it.substring(1)
+		}
+		return this.subList(3, 8)
 	}
-
-
 }
